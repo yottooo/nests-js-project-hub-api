f34d02b7c841c2026a965f13fd357f7c9ee68d1b
Yo <ednok2@abv.bg>
2026-09-15 18:29:16 +0300

    feat: replace Fastify with Express platform and update dependencies
    - Updated package.json to switch from @nestjs/platform-fastify to @nestjs/platform-express.
    - Updated @nestjs/platform-socket.io to version 11.2.5.
    - Added @types/express as a dependency.
    - Modified chat.module.ts to include ChatGateway as a provider.
    - Refactored main.ts to create the application without FastifyAdapter.
    - Updated e2e test to use INestApplication instead of NestFastifyApplication.
    - Created chat.gateway.ts and corresponding test file chat.gateway.spec.ts for WebSocket functionality.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
+import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: NestFastifyApplication;
+  let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication<NestFastifyApplication>(
-      new FastifyAdapter(),
-    );
+    app = moduleFixture.createNestApplication();
     await app.init();
-    await app.getHttpAdapter().getInstance().ready();
   });
 
   it('/ (GET)', () => {
@@ -29,6 +24,6 @@ describe('AppController (e2e)', () => {
   });
 
   afterEach(async () => {
-    await app?.close();
+    await app.close();
   });
 });
